0c3826a67f907bb277fa51b79832538b2b749de0
MariusREL <mrel0425@gmail.com>
2025-05-07 19:04:30 +0200

    add duck and fix links

--- about/about.js
+++ about/about.js
@@ -1,19 +1,3 @@
-// switch (browser) {
-//   case "Edge":
-//     alert("You've got the Edge!");
-//     break;
-
-//   case "Chrome":
-//   case "Firefox":
-//   case "Safari":
-//   case "Opera":
-//     alert("Okay we support these browsers too");
-//     break;
-
-//   default:
-//     alert("We hope that this page looks ok!");
-// }
-
 const browserButton = document.querySelector(".browserButton");
 browserButton.addEventListener("click", function () {
   let browser = prompt("Browser");
@@ -31,26 +15,13 @@ browserButton.addEventListener("click", function () {
   }
 });
 
-// let a = +prompt("a?", "FIll out a number here");
 
-// if (a == 0) {
-//   alert(0);
-// }
-// if (a == 1) {
-//   alert(1);
-// }
+const duckButton = document.getElementById("duckButton1")
 
-// if (a == 2 || a == 3) {
-//   alert("2,3");
-// }
+duckButton.addEventListener("mouseenter", () => {
+  duckButton.src = "/img/Duck02.svg";
+});
 
-
-const birthYear = 1948;
-const thisYear = 1965;
-const firstName = "Carlos";
-const lastName = "Stevenson";
-const fullName = firstName + " " + lastName;
-const age = (thisYear - birthYear)
-const greeting = `Hello! My name is $(fullname) and i am $(age) years old. `
-
-console.log(greeting);
+duckButton.addEventListener("mouseleave", () => {
+  duckButton.src = "/img/Duck01.svg"
+});
